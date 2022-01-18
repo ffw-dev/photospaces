3ede7294891e9951a99701e25d3feceee208b739
ffw-dev <93714234+ffw-dev@users.noreply.github.com>
2022-01-18 23:40:53 +0100

    Photos overview UI refactor.

--- lib/screens/photos_preview_screen.dart
+++ lib/screens/photos_preview_screen.dart
@@ -27,6 +27,7 @@ class _PhotosPreviewScreenState extends State<PhotosPreviewScreen> {
     return Scaffold(
         appBar: AppBar(),
         body: Column(children: [
+          if (isSelectMode) buildCancelSelectionModeButton(),
           Expanded(
             child: GridView.count(
               primary: false,
@@ -42,6 +43,10 @@ class _PhotosPreviewScreenState extends State<PhotosPreviewScreen> {
                             Navigator.push(context, MaterialPageRoute(builder: (_) => PhotoPreviewScreen(photo))),
                         onLongPress: () => setState(() {
                           isSelectMode = true;
+                          _selectedPhotos = [];
+                          if (_selectedPhotos.contains(photo)) {
+                            return;
+                          }
                           _selectedPhotos.add(photo);
                         }),
                         child: Stack(children: [
@@ -73,59 +78,128 @@ class _PhotosPreviewScreenState extends State<PhotosPreviewScreen> {
               ],
             ),
           ),
-            if (isSelectMode)
-              Padding(
-              padding: const EdgeInsets.all(24),
-              child: GestureDetector(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(24.0),
-                      child: Text(
-                        'Upload ${_selectedPhotos.length} photos',
-                        style: const TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    const Icon(
-                      Icons.save,
-                      size: 24,
-                      color: Colors.red,
-                    )
-                  ],
-                ),
-                onTap: () async {
-                  if (_selectedPhotos.isEmpty) {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text('Enable selection mode by tapping and holding a photo')));
-                    return;
-                  }
-
-                  successFullUploadsCount = 0;
-                  ScaffoldMessenger.of(context).showSnackBar(uploadingSnackBar());
-                  await uploadPhotos();
-                  ScaffoldMessenger.of(context).showSnackBar(successfulUploadSnackBar());
-                },
-              ),
+          if (isSelectMode && _selectedPhotos.isNotEmpty) buildUploadNumberOfPhotosButton(context),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (!isSelectMode) buildUploadAllButton(context),
+                if (_selectedPhotos.length != widget._photos.length && isSelectMode) buildSelectAllButton(),
+                if (isSelectMode && _selectedPhotos.isNotEmpty) buildUnselectAllButton()
+              ],
             ),
-          if (!isSelectMode)
-            TextButton(
-                onPressed: () async {
-                  _selectedPhotos = widget._photos;
-                  await uploadPhotos();
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(24.0),
-                  child: Text('Upload all', style: TextStyle(fontSize: 18),),
-                )
-            )
-
+          )
         ]));
   }
 
+  TextButton buildUnselectAllButton() {
+    return TextButton(
+        onPressed: () async {
+          setState(() {
+            _selectedPhotos = [];
+          });
+        },
+        child: const Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Text(
+            'Unselect all',
+            style: TextStyle(fontSize: 14),
+          ),
+        ));
+  }
+
+  TextButton buildSelectAllButton() {
+    return TextButton(
+        onPressed: () async {
+          setState(() {
+            _selectedPhotos = List.from(widget._photos);
+            isSelectMode = true;
+          });
+        },
+        child: const Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Text(
+            'Select all',
+            style: TextStyle(fontSize: 14),
+          ),
+        ));
+  }
+
+  TextButton buildUploadAllButton(BuildContext context) {
+    return TextButton(
+        onPressed: () async {
+          _selectedPhotos = widget._photos;
+          await handleUploadAndShowSnackBars(context);
+        },
+        child: const Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Text(
+            'Upload all',
+            style: TextStyle(fontSize: 16),
+          ),
+        ));
+  }
+
+  Padding buildUploadNumberOfPhotosButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: GestureDetector(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Upload ${_selectedPhotos.length} photos',
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            const Icon(
+              Icons.save,
+              size: 24,
+              color: Colors.red,
+            )
+          ],
+        ),
+        onTap: () async {
+          if (_selectedPhotos.isEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('No photos selected')));
+            return;
+          }
+
+          successFullUploadsCount = 0;
+          await handleUploadAndShowSnackBars(context);
+        },
+      ),
+    );
+  }
+
+  Future<void> handleUploadAndShowSnackBars(BuildContext context) async {
+    ScaffoldMessenger.of(context).showSnackBar(showSnackBarWithText('Uploading, please wait'));
+    await uploadPhotos();
+    ScaffoldMessenger.of(context).showSnackBar(showSnackBarWithText('Successfully uploaded $successFullUploadsCount of ${_selectedPhotos.length} pictures.'));
+  }
+
+  TextButton buildCancelSelectionModeButton() {
+    return TextButton(
+        onPressed: () async {
+          setState(() {
+            _selectedPhotos = [];
+            _selectedPhotos = widget._photos;
+            isSelectMode = false;
+          });
+        },
+        child: const Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Text(
+            'cancel selection mode',
+            style: TextStyle(fontSize: 14, color: Colors.red),
+          ),
+        ));
+  }
+
   Future<void> uploadPhotos() async {
+    successFullUploadsCount = 0;
     for (var file in _selectedPhotos) {
       await DevEzaApi.ezFileEndpoints
           .uploadPost(FormData.fromMap({
@@ -134,11 +208,10 @@ class _PhotosPreviewScreenState extends State<PhotosPreviewScreen> {
         "File": await MultipartFile.fromFile(file.path)
       }))
           .then((value) {
-        print(value);
         setState(() {
           successFullUploadsCount++;
         });
-      }).catchError((e) => print(e));
+      });
     }
   }
 
@@ -148,13 +221,9 @@ class _PhotosPreviewScreenState extends State<PhotosPreviewScreen> {
         })
       : null;
 
-  SnackBar successfulUploadSnackBar() {
+  SnackBar showSnackBarWithText(String text) {
     return SnackBar(
-      content: Text('Successfully uploaded $successFullUploadsCount  of ${_selectedPhotos.length} pictures.'),
-      action: SnackBarAction(
-        label: 'Ok',
-        onPressed: () {},
-      ),
+      content: Text(text),
     );
   }
 
