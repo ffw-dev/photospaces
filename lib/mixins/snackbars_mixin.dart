a021ebd47254e60316db659d0c52e1316516ea13
ffw-dev <93714234+ffw-dev@users.noreply.github.com>
2022-01-31 15:19:20 +0100

    Adjusted duration of a snackbar.

--- lib/mixins/snackbars_mixin.dart
+++ lib/mixins/snackbars_mixin.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 mixin SnackBarsMixin {
   ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackBarWithText(BuildContext context, String text) {
     return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(milliseconds: 500),
       content: Text(text),
     ));
   }
